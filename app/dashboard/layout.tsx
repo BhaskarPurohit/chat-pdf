3c0c5af8bba57e902dd6c0beae99a5d9f4f5c72e
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-07-30 22:52:07 +0530

    header

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,14 @@
+import Header from '@/components/Header'
 import { ClerkLoaded } from '@clerk/nextjs'
 import React from 'react'
 
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
     <ClerkLoaded>
-    <div>{children}</div>
+    <div className='flex flex-col flex-1'>
+        <Header/>
+        {children}
+    </div>
 </ClerkLoaded>
   )
 }
