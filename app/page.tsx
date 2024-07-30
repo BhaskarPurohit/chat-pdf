2a9103eb5bc381f70e1980ff20b7a2578788f2d0
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-07-30 22:37:09 +0530

    adding dl section and margins, display grid, grid cols, adding mobile responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -5,32 +5,32 @@ import Link from "next/link";
 
 const features = [
   {
-    name:"Store your PDF Documents",
-    description:"Keep all your important PDF files securely stored and easily accessible anytime, anywhere",
-    icon:GlobeIcon
+    name: "Store your PDF Documents",
+    description: "Keep all your important PDF files securely stored and easily accessible anytime, anywhere",
+    icon: GlobeIcon
   },
 
   {
-    name:"Blazing Fast Responses",
-    description:"Experience lightning-fast answers to your queries, ensuring you get the information you need instantly.",
+    name: "Blazing Fast Responses",
+    description: "Experience lightning-fast answers to your queries, ensuring you get the information you need instantly.",
     icon: ZapIcon
   },
 
   {
-    name:"Chat memorization",
-    description:"Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
+    name: "Chat memorization",
+    description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
     icon: BrainCogIcon
   },
 
   {
-    name:"Interactive PDF Viewer",
-    description:"Engage with your PDFs like never before using our intuitive and interactive viewer.",
+    name: "Interactive PDF Viewer",
+    description: "Engage with your PDFs like never before using our intuitive and interactive viewer.",
     icon: EyeIcon
   },
 
   {
-    name:"Responsive Across Devices",
-    description:"Access and chat with your PDFs seamlessly on any device, whether it's your desktop, tablet or smartphone.",
+    name: "Responsive Across Devices",
+    description: "Access and chat with your PDFs seamlessly on any device, whether it's your desktop, tablet or smartphone.",
     icon: MonitorSmartphoneIcon
   }
 ]
@@ -40,7 +40,7 @@ export default function Home() {
     <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-indigo-600">
       <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
         <div className="flex flex-col items-center mx-auto max-w-7xl px-6 lg:px-8">
-           <div className="mx-auto max-w-2xl sm:text-center">
+          <div className="mx-auto max-w-2xl sm:text-center">
             <h2 className="text-base font-semibold leading-7 text-indigo-600">
               Your Interactive Document Companion
             </h2>
@@ -56,19 +56,19 @@ export default function Home() {
               <span className="font-bold">dynamic conversations</span>,
               enhancing productivity 10x fold effortlessly
             </p>
-           </div>
-           <Button asChild className="mt-10">
+          </div>
+          <Button asChild className="mt-10">
             <Link href="/dashboard">Get Started</Link>
-           </Button>
+          </Button>
         </div>
         <div className="relative overflow-hidden pt-16">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <Image
-            alt="app screenshot"
-            src="https://i.imgur.com/VciRSTI.jpeg"
-            width={2432}
-            height={1442}
-            className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+              alt="app screenshot"
+              src="https://i.imgur.com/VciRSTI.jpeg"
+              width={2432}
+              height={1442}
+              className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
             />
             <div aria-hidden="true" className="relative">
               <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]"></div>
@@ -76,17 +76,19 @@ export default function Home() {
           </div>
         </div>
         <div className="">
-          {features.map(feature => (
-            <div className="relative pl-9">
-              <dt className="inline font-semibold text-gray-900">
-                <feature.icon
-                aria-hidden="true"
-                className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
-                />
-              </dt>
-              <dd>{feature.description}</dd>
-            </div>
-          ))}
+          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-abse leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+            {features.map(feature => (
+              <div className="relative pl-9">
+                <dt className="inline font-semibold text-gray-900">
+                  <feature.icon
+                    aria-hidden="true"
+                    className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                  />
+                </dt>
+                <dd>{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
         </div>
       </div>
     </main>
