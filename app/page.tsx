795d318db16bb530526646aef508b6402729a71e
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-07-30 22:40:39 +0530

    setting info cards with proper div

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="">
+        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
           <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-abse leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
             {features.map(feature => (
               <div className="relative pl-9">
