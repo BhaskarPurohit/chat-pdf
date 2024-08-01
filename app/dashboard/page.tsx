b1183754ae8cd6c47b68bb31315937cee39ef825
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-08-01 11:45:32 +0530

    placeholder document

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,18 @@
+// "use client"
+export const dynamic = "force-dynamic"
+import Documents from '@/components/Documents'
 import { ClerkLoaded } from '@clerk/nextjs'
 import React from 'react'
 
+
 const Dashboard = ({children}:{children:React.ReactNode}) => {
   return (
-   <div>dashboard</div>
+   <div className='h-full max-w-7xl mx-auto'>
+    <h1 className='text-3xl p-5 bg-gray-100 font-extralight text-indigo-600'>
+        My Documents
+        </h1>
+        <Documents/>
+   </div>
   )
 }
 
---
+++ components/Documents.tsx
@@ -0,0 +1,13 @@
+function Documents(){
+    return(
+        <div
+        className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto"
+        >
+            {/* map thorugh documents */}
+
+            {/* placeholder document */}
+        </div>
+    )
+}
+
+export default Documents
